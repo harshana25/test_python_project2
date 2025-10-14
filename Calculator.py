75008685aa604437cc187207c13b8d576af194fb
Lahiru Harshana Heenatigala <138371112+harshana25@users.noreply.github.com>
2025-10-14 14:14:16 +0300

    Initial Commit

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+print("Welcome to simple calculator")
+
+# input first number from user
+number_1 = input("Input first number\n")
+number_1 = float(number_1)
+
+# input second number from user
+number_2 = input("Input second number\n")
+number_2 = float(number_2)
+
+# input a function from user
+ask_the_function =input('Please select the operation (+,/,*,-)\n')
